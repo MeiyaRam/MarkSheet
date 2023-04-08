5c2d24ca317d0e61398c9fbf0436012cda8513cf
MeiyaRam <MeiyaRam@gmail.com>
2023-04-08 13:00:57 +0530

    Introduced to background style in button with map.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ const App = (context) => {
 	return <div className="App">
 		<b><i>Ready To Start.</i></b><br/>
 		<button style={ { color: 'red' } }>Click Me!</button>
-		{colors.map((color) => <button key={ color }>{color}</button>)}
+		{colors.map((color) =>
+			<button
+				key={ color }
+				style={ { background: color } }
+			>{color}
+			</button>)}
 	</div>;
 };
 
