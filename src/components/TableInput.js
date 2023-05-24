1bb499db3f56d0ce6b11a4efd28a2205e46c3935
MeiyaRam <MeiyaRam@gmail.com>
2023-05-24 17:09:00 +0530

    Modified code for hide total,result and rank textbox data map from config.

--- src/components/TableInput.js
+++ src/components/TableInput.js
@@ -1,22 +1,30 @@
 import { values, map } from '@laufire/utils/collection';
 import { React } from 'react';
 
+const getClassName = (key) => (
+	key === 'total' || key === 'result' || key === 'rank'
+		? 'hideText'
+		: 'showText');
+
 const TableInput = (context) => {
-	const {
-		actions, config: { markSheetData }, state,
-	} = context;
+	const { actions, config: { markSheetData }, state } = context;
 
 	return (
-		values(map(markSheetData, ({ title, type }, key) =>
-			<div key={ key }>
-				<label>Enter Your {title} </label>
-				<input
-					type={ type }
-					value={ state.markSheet[key] }
-					onChange={ ({ target: { value }}) => actions
-						.setMarkSheetInput({ label: key, value: value }) }
-				/>
-			</div>))
+		values(map(markSheetData, ({ title, type }, key) => {
+			const className = getClassName(key);
+
+			return (
+				<div key={ key }>
+					<label className={ className }>
+						Enter Your {title} </label>
+					<input
+						type={ type }
+						className={ className }
+						value={ state.markSheet[key] }
+						onChange={ ({ target: { value }}) => actions
+							.setMarkSheetInput({ label: key, value: value }) }
+					/></div>);
+		}))
 	);
 };
 
